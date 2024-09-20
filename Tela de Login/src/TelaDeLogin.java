d2089e1a858d5ff5715bb130a78a805bca5e7548
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-09-19 21:47:21 -0300

    SCRUM-40 | Adicionado Tela de Login

---
+++ Tela de Login/src/TelaDeLogin.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package tela.de.login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author parko
+ */
+public class TelaDeLogin extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Front Login.fxml"));
+        
+        Scene scene = new Scene(root, 800, 600);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
