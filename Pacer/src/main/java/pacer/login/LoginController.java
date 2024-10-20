ef5d048c4cb2cf121ad422330adf120aa02ab01f
Luiz Ricardo <lu.iz.ricardo@hotmail.com>
2024-10-20 19:02:09 -0300

    SCRUM-57 | Finalizada função de Login

--- Pacer/src/main/java/pacer/login/LoginController.java
+++ Pacer/src/main/java/pacer/login/LoginController.java
@@ -5,14 +5,24 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import pacer.data.dao.AlunoDAO;
+import pacer.data.dao.ProfessorDAO;
+import pacer.data.models.Aluno;
+import pacer.data.models.Professor;
+import pacer.utils.mbox;
+import pacer.utils.sceneSwitcher;
 
 public class LoginController implements Initializable {
+
+     @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -20,14 +30,29 @@ public class LoginController implements Initializable {
 
     @FXML
     private void handleLogin(javafx.event.ActionEvent event) throws IOException {
+        String email = emailField.getText();
+        String senha = passwordField.getText();
+        String fxmlPath;
+        
+        // verifica se é um aluno (se nao for se torna null)
+        Aluno aluno = AlunoDAO.findByEmailAndSenha(email, senha);
+        // verifica se é um professor (se nao for se torna null)
+        Professor professor = ProfessorDAO.findByEmailAndSenha(email, senha);
 
-        // TODO verificaçao de login com o LoginDAO
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/AlunoHomeView.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        Stage stage;
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        // se aluno nao é nulo, portanto o email e senha digitados correspondem a um aluno (o mesmo vale pro professor)
+        if (aluno != null ) {
+            fxmlPath = "/FXML/AlunoHomeView.fxml";
+        }
+        else if (professor != null) {
+            fxmlPath = "/FXML/ProfHomeView.fxml";
+        }
+        // se nao é nenhum dos dois emite uma mensagem de credenciais invalidas
+        else {
+            mbox.ShowMessageBox(AlertType.WARNING, "Erro ao efetuar login.", "Usuário ou senha incorretos.");
+            return;
+        }
+
+        // função que abre a tela
+        sceneSwitcher.switchScene(fxmlPath, event);
     }
 }
