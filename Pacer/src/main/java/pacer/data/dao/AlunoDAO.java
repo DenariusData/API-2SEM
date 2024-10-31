0bcf8e88aaf27449927bd00fd53b774e3f6cada4
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-10-30 21:38:02 -0300

    Merge branch 'SPRINT-3' of https://github.com/DenariusData/API-2SEM into SPRINT-3

--- Pacer/src/main/java/pacer/data/dao/AlunoDAO.java
+++ Pacer/src/main/java/pacer/data/dao/AlunoDAO.java
@@ -16,13 +16,14 @@ public class AlunoDAO {
     private static Connection connection = DatabaseConnection.getConnection();
 
     public static void addAluno(Aluno aluno) {
-        String sql = "INSERT INTO ALUNO (ALUNO_RA, ALUNO_EMAIL, ALUNO_NOME, ALUNO_SENHA, FOTO) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO ALUNO (ALUNO_RA, ALUNO_EMAIL, ALUNO_NOME, ALUNO_SENHA, FOTO, GRUPO_ID) VALUES (?, ?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setLong(1, aluno.getRa());
             stmt.setString(2, aluno.getEmail());
             stmt.setString(3, aluno.getNome());
             stmt.setString(4, aluno.getSenha());
             stmt.setBlob(5, new ByteArrayInputStream(aluno.getFoto()));
+            stmt.setInt(6, aluno.getGrupoId());
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -41,7 +42,9 @@ public class AlunoDAO {
                     rs.getString("ALUNO_EMAIL"), 
                     rs.getString("ALUNO_NOME"), 
                     rs.getString("ALUNO_SENHA"), 
-                    rs.getBytes("FOTO"));
+                    rs.getBytes("FOTO"),
+                    rs.getInt("GRUPO_ID")
+                );
                 return alunoLogado;
             }
         } catch (SQLException e) {
@@ -55,8 +58,14 @@ public class AlunoDAO {
         String sql = "SELECT * FROM ALUNO";
         try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
-                alunos.add(new Aluno(rs.getLong("ALUNO_RA"), rs.getString("ALUNO_EMAIL"),
-                        rs.getString("ALUNO_NOME"), rs.getString("ALUNO_SENHA"), rs.getBytes("FOTO")));
+                alunos.add(new Aluno(
+                    rs.getLong("ALUNO_RA"), 
+                    rs.getString("ALUNO_EMAIL"),
+                    rs.getString("ALUNO_NOME"), 
+                    rs.getString("ALUNO_SENHA"), 
+                    rs.getBytes("FOTO"), 
+                    rs.getInt("GRUPO_ID")
+                ));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -65,13 +74,14 @@ public class AlunoDAO {
     }
 
     public static void updateAluno(Aluno aluno) {
-        String sql = "UPDATE ALUNO SET ALUNO_EMAIL = ?, ALUNO_NOME = ?, ALUNO_SENHA = ?, FOTO = ? WHERE ALUNO_RA = ?";
+        String sql = "UPDATE ALUNO SET ALUNO_EMAIL = ?, ALUNO_NOME = ?, ALUNO_SENHA = ?, FOTO = ?, GRUPO_ID = ? WHERE ALUNO_RA = ?";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, aluno.getEmail());
             stmt.setString(2, aluno.getNome());
             stmt.setString(3, aluno.getSenha());
             stmt.setBlob(4, new ByteArrayInputStream(aluno.getFoto()));
-            stmt.setLong(5, aluno.getRa());
+            stmt.setInt(5, aluno.getGrupoId());
+            stmt.setLong(6, aluno.getRa());
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
