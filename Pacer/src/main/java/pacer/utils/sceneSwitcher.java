f932330297588ae2c07a4ae142fe9223f6b0ba4c
Thiago <teagos371@gmail.com>
2024-10-31 23:07:45 -0300

    SCRUM-82 - Adicionado novo metodo switchScene na utils

--- Pacer/src/main/java/pacer/utils/sceneSwitcher.java
+++ Pacer/src/main/java/pacer/utils/sceneSwitcher.java
@@ -22,4 +22,17 @@ public class sceneSwitcher {
         stage.centerOnScreen();
         stage.show();
     }
+    public static <T> T switchSceneRetController(String fxmlPath, ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(sceneSwitcher.class.getResource(fxmlPath));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+        
+        return loader.getController(); 
+    }
 }
