98ab0636e2b125406faa7c9720a619bc759b67a4
Thiago <teagos371@gmail.com>
2024-10-31 21:43:52 -0300

    SCRUM-77 - Corrigido relação de onAction's
    Co-Authored-By: Luiz Ricardo <163085805+luizricar@users.noreply.github.com>
    Co-Authored-By: Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>

--- Pacer/src/main/java/pacer/professor/ProfEquipesController.java
+++ Pacer/src/main/java/pacer/professor/ProfEquipesController.java
@@ -16,5 +16,44 @@ public class ProfEquipesController {
     public void confirmarVoltar(ActionEvent event) throws IOException {
         sceneSwitcher.switchScene("/FXML/ProfHomeView.fxml", event);
     }
-    
+    @FXML
+    public void abrirImport(ActionEvent event) throws IOException {
+        sceneSwitcher.switchScene("/FXML/ProfImportarView.fxml", event);
+    }
+    @FXML
+    public void adicionarGrupo(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void editarGrupo(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void deletarGrupo(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void limparCamposGrupo(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void adicionarIntegrante(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void editarIntegrante(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void deletarIntegrante(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void limparCamposIntegrante(ActionEvent event) throws IOException {
+        
+    }
+    @FXML
+    public void baixarRelatorio(ActionEvent event) throws IOException {
+        
+    }
 }
