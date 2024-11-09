5953e19f252e189981d090dd05f6eccd195a8bed
Thiago <teagos371@gmail.com>
2024-11-09 20:37:09 -0300

    SCRUM-82 - Reorganizado pacer.Main / Criado App.java

---
+++ Pacer/src/main/java/pacer/App.java
@@ -0,0 +1,38 @@
+package pacer;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/FXML/ProfEquipesView.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        
+        stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        try {
+            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
